c2c3107422069c220a5104032027a89e8f7b1201
hotelcurtains <114700124+hotelcurtains@users.noreply.github.com>
2024-10-20 20:00:29 -0400

    atoi

--- hw2/make.bash
+++ hw2/make.bash
@@ -1,10 +1,10 @@
 #!/bin/bash
-#aarch64-linux-gnu-as bins.s -g -o bins.o
-#aarch64-linux-gnu-as bins_data.s -g -o bins_data.o
-#aarch64-linux-gnu-ld bins.o bins_data.o
-#qemu-aarch64 -g 1235 a.out
+aarch64-linux-gnu-as atoi.s -g -o atoi.o
+aarch64-linux-gnu-as atoi_data.s -g -o atoi_data.o
+aarch64-linux-gnu-ld atoi.o atoi_data.o
+qemu-aarch64 -g 1235 a.out
 
-aarch64-linux-gnu-as bins.s -o bins.o
-aarch64-linux-gnu-as bins_data.s -o bins_data.o
-aarch64-linux-gnu-ld bins.o bins_data.o -o bins
-qemu-aarch64 bins
+#aarch64-linux-gnu-as bins.s -o bins.o
+#aarch64-linux-gnu-as bins_data.s -o bins_data.o
+#aarch64-linux-gnu-ld bins.o bins_data.o -o bins
+#qemu-aarch64 bins
