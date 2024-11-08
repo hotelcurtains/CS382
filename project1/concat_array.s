20be363b9870b70a319e4bcacd095e17799c1608
hotelcurtains <114700124+hotelcurtains@users.noreply.github.com>
2024-11-07 20:49:23 -0500

    WIP

--- project1/concat_array.s
+++ project1/concat_array.s
@@ -1,15 +1,68 @@
-
-/*  Name:
-    Pledge: 
+/*  
+   Name: Daniel Detore
+   Pledge: I pledge my honor that I have abided by the Stevens Honor System.
  */
 
-.global concat_array
 
+//.global concat_array
+.extern itoascii
+.text
+.global _start
+_start:
+
+// char* concat_array(unsigned long int* arr, unsigned long int len);
 concat_array:
+SUB SP, SP, 8           // make room for X30
+STR X30, [SP]           // store X30
+
+// TODO FOR TESTING ONLY
+ADR X0, test_arr
+MOV X1, 4
+
+// assume XO = arr[0], X1 = len
+// SAVE ALL OF THESE TO STACK
+MOV X8, 0                  // X8 = output index
+MOV X9, 0                  // X9 = array index
+MOV X10, 32                // X10 = ascii space
+ADR X16, concat_array_outstr // X16 =  &concat_array_outstr[0]
+MOV X17, X0                // X17 = &arr[0] (needs to be preserved through function calls)
+SUB X18, X1, 1             // X18 = max index of arr
+
+convertloop:
+   LDR X0, [X17, X9]       // X0 = &arr[0] + array index
+   BL itoascii             // X0 = itoascii(arr[i])
+
+   // for (char j : itoascii(arr[i])) output += char
+   MOV X11, 0              // X11 = interator
+   concatloop:
+      LDRB W12, [X0, X11]  // X12 = j
+      CBZ X12, concatend   // if (j == "\0") break
+      STRB W12, [X16, X8]  // concat_array_outstr[output index] = j
+      ADD X8, X8, 1        // output index++
+      ADD X11, X11, 1      // iterator++
+      B concatloop         // else continue
+
+   concatend:
+   STRB W10, [X16, X8]     // append space
+   ADD X8, X8, 1           // output index++
+   ADD X9, X9, 8           // array index += 8; long ints are 8 bytes long
+
+   CMP X9, X18             // array index =? max index
+   B.EQ convertend         // if (array index == max index) break
+   B convertloop           // else continue
+
+convertend:
+STRB WZR, [X16, X8]        // concat_array_outstr[-1] = "\0"
+MOV X0, X16                // return &concat_array_outstr[0]
+
+LDR X30, [SP]              // load X30
+ADD SP, SP, 8              // move SP back
+RET	                     // return 
+
 
 
 .data
     /* Put the converted string into concat_array_outstrer,
        and return the address of concat_array_outstr */
     concat_array_outstr:  .fill 1024, 1, 0
-
+   test_arr: .quad 69, 69, 69, 69
--- project1/itoascii.s
+++ project1/itoascii.s
@@ -6,19 +6,23 @@
 
 .global itoascii
 
+.text
+
+_start:
 // char* itoascii(unsigned long int number);
 itoascii:
-   SUB SP, SP, 8           // make room for X30
-   STR X30, [SP]           // store X30
+   SUB SP, SP, 8            // make room for X30
+   STR X30, [SP]            // store X30
 
-   // X0 = number
-   MOV X1, X0              // X1 = number
-   MOV X5, 0               // X5 = length of number
-   count:                  // iteratively count the amount of digits in number
-      UDIV X1, X1, 10      // num /= 10
-      ADD X5, X5, 1        // length++
-      CBZ X1, stopcount    // if (num = 0) break
-      B count              // else continue
+   // expect X0 = number
+   MOV X1, X0               // X1 = number
+   MOV X5, 0                // X5 = length of decimal number
+   MOV X18, 10              // X18 = 10 for MUL & UDIV so the assembler stops crying
+   count:                   // iteratively count the amount of digits in number
+      UDIV X1, X1, X18      // num /= 10
+      CBZ X1, stopcount     // if (num = 0) break
+      ADD X5, X5, 1         // length++
+      B count               // else continue
 
    stopcount:
    MOV X10, 0               // X10 = i = 0, i < amount of digits in number
@@ -27,8 +31,8 @@ itoascii:
    separate:
       // X1 = number without least decimal digit
       MOV X1, X0            // X1 = number
-      UDIV X1, X1, 10       // X1 = floor(number/10)
-      MUL X1, X1, 10        // X1 = floor(number/10)*10
+      UDIV X1, X1, X18      // X1 = floor(number/10)
+      MUL X1, X1, X18       // X1 = floor(number/10)*10
 
       // X1 = only least decimal digit of number
       SUB X1, X0, X1        // remove all digits except 1s from X1
@@ -38,22 +42,15 @@ itoascii:
 
       MOV X4, 0             // X4 = storage offset
       SUB X4, X5, X10       // X4 = len(number) - i
-      STR X1, [X11, X4]     // store char[i] into buffer[i]
+      STRB W1, [X11, X4]    // store char[i] into buffer[i]
 
-      DIV X0, X0, 10        // number /= 10
+      UDIV X0, X0, X18      // number /= 10
       ADD X10, X10, 1       // i++
       CBZ X0, stopsep       // if (number = 0) break
       B    separate         // else continue
 
    stopsep:
-      
-
-   exit:
-      MOV  X0, 0
-      MOV  X8, 93
-      SVC  0
-
-
+   ADR X0, buffer          // return address of resulting string
 
    LDR X30, [SP]           // load X30
    ADD SP, SP, 8           // move SP back
