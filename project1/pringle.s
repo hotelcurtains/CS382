8ea94c325115f5a595f481cced06090fd2746706
hotelcurtains <114700124+hotelcurtains@users.noreply.github.com>
2024-11-11 23:58:02 -0500

    die

--- project1/pringle.s
+++ project1/pringle.s
@@ -1,13 +1,29 @@
 
-/*  Name:
-    Pledge:
+/*  
+   Name: Daniel Detore
+   Pledge: I pledge my honor that I have abided by the Stevens Honor System.
  */
+
 
 .global pringle
 
 pringle:
 
 
+// X0 = &str
+// X1, 3, 5, 7 = &arr_n
+// X2, 4, 6 = len(arr_n)
+// SP
+
+
+SUB SP, SP,  8             // make room for X30
+STR X30, [SP]              // store X30
+
+
+
+LDR X30, [SP]              // load X30
+ADD SP, SP, 48             // move SP back
+RET	                       // return 
 
 
 /*
